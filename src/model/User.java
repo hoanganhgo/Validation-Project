ccbed9e2ae61c4d8140458ee667734e18c66171f
1712276+LuongHoangAnh <1712276+LuongHoangAnh@users.noreply.github.com>
2021-01-17 23:29:35 +0700

    Update UI

--- src/model/User.java
+++ src/model/User.java
@@ -1,4 +1,41 @@
 package model;
 
+import framework.annotation.NotNull;
+import framework.annotation.Regex;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class User {
+
+    @Regex(pattern = "^[A-Za-z0-9+_.-]+@(.+)$")
+    private String email;
+
+    private String phone;
+
+    private String password;
+
+    private Date dob;
+
+    public User(String email, String phone, String password, String dob) {
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+
+        try {
+            this.dob = new SimpleDateFormat("dd/MM/yyyy").parse(dob);
+        } catch (ParseException e) {
+            this.dob = new Date();
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        result.append("Email: ").append(this.email).append("\n");
+        result.append("Phone: ").append(this.phone).append("\n");
+        result.append("Day of birth: ").append(this.dob.toString()).append("\n");
+        return result.toString();
+    }
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,20 +1,23 @@
-/*
 package ui;
 
+import framework.validation.ConstraintViolation;
+import framework.validation.Validation;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import model.User;
+
+import java.util.Set;
 
 public class Main extends Application {
 
@@ -35,7 +38,7 @@ public class Main extends Application {
         title.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
         title.setTextFill(Color.BLUE);
 
-        Label labelEmail = new Label("notify error");
+        Label labelEmail = new Label();
         labelEmail.setFont(Font.font("Verdana", FontWeight.NORMAL, 12));
         labelEmail.setTextFill(Color.RED);
 
@@ -44,7 +47,7 @@ public class Main extends Application {
         fieldEmail.setPrefSize(400,30);
         fieldEmail.setFont(Font.font("Verdana", FontWeight.NORMAL, 20));
 
-        Label labelPhoneNumber = new Label("notify error");
+        Label labelPhoneNumber = new Label();
         labelPhoneNumber.setFont(Font.font("Verdana", FontWeight.NORMAL, 12));
         labelPhoneNumber.setTextFill(Color.RED);
 
@@ -53,7 +56,7 @@ public class Main extends Application {
         fieldPhoneNumber.setPrefSize(400,30);
         fieldPhoneNumber.setFont(Font.font("Verdana", FontWeight.NORMAL, 20));
 
-        Label labelPassword = new Label("notify error");
+        Label labelPassword = new Label();
         labelPassword.setFont(Font.font("Verdana", FontWeight.NORMAL, 12));
         labelPassword.setTextFill(Color.RED);
 
@@ -62,7 +65,7 @@ public class Main extends Application {
         fieldPassword.setPrefSize(400,30);
         fieldPassword.setFont(Font.font("Verdana", FontWeight.NORMAL, 20));
 
-        Label labelDoB = new Label("notify error");
+        Label labelDoB = new Label();
         labelDoB.setFont(Font.font("Verdana", FontWeight.NORMAL, 12));
         labelDoB.setTextFill(Color.RED);
 
@@ -73,6 +76,42 @@ public class Main extends Application {
 
         Button signIn = new Button("Submit");
         signIn.setFont(Font.font("Verdana", FontWeight.NORMAL, 20));
+        signIn.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            User user = new User(fieldEmail.getText(), fieldPhoneNumber.getText(), fieldPassword.getText(),
+                    fieldDoB.getText());
+
+            Validation validation = Validation.getInstance();
+            Set<ConstraintViolation> violations = validation.validate(user);
+
+            String notifyEmail = "";
+            String notifyPhone = "";
+            String notifyPassword = "";
+            String notifyDateOfBirth = "";
+
+            for (ConstraintViolation violation : violations) {
+                switch (violation.getProperty()) {
+                    case "email":
+                        notifyEmail += violation.getMessage() + ", ";
+                        break;
+                    case "phone":
+                        notifyPhone += violation.getMessage() + ", ";
+                        break;
+                    case "password":
+                        notifyPassword += violation.getMessage() + ", ";
+                        break;
+                    case "dob":
+                        notifyDateOfBirth += violation.getMessage() + ", ";
+                        break;
+                }
+
+                System.out.println(violation.getProperty() + "->" + violation.getMessage());
+            }
+
+            labelEmail.setText(notifyEmail);
+            labelPhoneNumber.setText(notifyPhone);
+            labelPassword.setText(notifyPassword);
+            labelDoB.setText(notifyDateOfBirth);
+        });
 
         gridPane.add(title, 0, 0);
         gridPane.add(labelEmail, 0, 1);
@@ -92,4 +131,3 @@ public class Main extends Application {
         launch(args);
     }
 }
-*/
